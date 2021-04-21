16839e646d335e2b098078f87bddd16afaddcee2
Rahul Ramesh <rr6004@gmail.com>
2021-04-21 11:54:56 -0400

    working on testing

--- PCA_metrics.py
+++ PCA_metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def subspace_dist(A_1, A_2):
-    return np.square(np.linalg.norm(A_1 - A_2, ord="fro"))
+    return np.linalg.norm(A_1 - A_2, ord="fro")
 
 #def spectral_norm(A_1, A_2):
     
